16a33f9d69f4fdde1b513255e696e2658ec31da4
Jonathan Canfield <jonathantcanfield@gmail.com>
2018-10-20 23:13:46 -0400

    fix issue where app would error if status was not granted for push notifications

--- components/subcomponents/pushNotification.js
+++ components/subcomponents/pushNotification.js
@@ -3,14 +3,21 @@ import axios from '../../apiClient';
 
 export default async function registerForPushNotificationsAsync(appStore) {
     const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+    let finalStatus = existingStatus;
 
     // only ask if permissions have not already been determined, because
     // iOS won't necessarily prompt the user a second time.
     if (existingStatus !== 'granted') {
         // Android remote notification permissions are granted during the app
         // install, so this will only ask on iOS
-        await Permissions.askAsync(Permissions.NOTIFICATIONS);
+        const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+        finalStatus = status;
     };
+
+    // Stop here if the user did not grant permissions
+    if (finalStatus !== 'granted') {
+        return;
+    }
 
     try {
         // Get the token that uniquely identifies this device
