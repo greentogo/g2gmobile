d812daf0b41f416adaad194b9ae36bb58b147e63
Jonathan Canfield <jonathantcanfield@gmail.com>
2018-11-04 17:14:30 -0500

    lint AccountScreen

--- components/AccountScreen.js
+++ components/AccountScreen.js
@@ -3,30 +3,30 @@ import {
     WebView,
     Linking,
 } from 'react-native';
-import { inject, observer } from "mobx-react";
-import styles from "../styles";
+import { inject, observer } from 'mobx-react';
 import {
     Content,
     List,
-} from "native-base";
+} from 'native-base';
+import styles from '../styles';
 import ListMenuItem from './subcomponents/ListMenuItem';
 import SubscriptionBanner from './subcomponents/SubscriptionBanner';
-import CommunityBoxes from "./subcomponents/CommunityBoxes";
+import CommunityBoxes from './subcomponents/CommunityBoxes';
 
-@inject("appStore")
+@inject('appStore')
 @observer
 class AccountScreen extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            ...this.props.appStore.user,
-            redirectToWeb: false
-        }
-    }
-
     static navigationOptions = {
         title: 'Account',
     };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            redirectToWeb: false,
+        };
+    }
+
 
     goToNameAndEmail = () => {
         this.props.navigation.navigate('editnameemail');
@@ -34,48 +34,47 @@ class AccountScreen extends React.Component {
 
     render() {
         if (this.state.redirectToWeb) {
-            let uri = this.state.redirectToWeb;
+            const uri = this.state.redirectToWeb;
             return (
                 <WebView
                     ref={(ref) => { this.webview = ref; }}
                     source={{ uri }}
                     onNavigationStateChange={(event) => {
-                        this.setState({ redirectToWeb: false })
+                        this.setState({ redirectToWeb: false });
                         Linking.openURL(event.url);
                         this.webview.stopLoading();
                     }}
                 />
             );
-        } else {
-            return (
-                <Content style={styles.container}>
-                    <List>
-                        <ListMenuItem
-                            icon="person"
-                            text="View/Edit Name and Email"
-                            onPress={this.goToNameAndEmail}
-                        />
-                        <ListMenuItem
-                            icon="credit-card"
-                            text="Update payment method"
-                            onPress={() => { this.setState({ redirectToWeb: 'https://app.durhamgreentogo.com/account/change_payment_method/' }) }}
-                        />
-                        <ListMenuItem
-                            icon="inbox"
-                            text="View/Edit Subscriptions"
-                            onPress={() => { this.setState({ redirectToWeb: 'https://app.durhamgreentogo.com/subscriptions/' }) }}
-                        />
-                        <ListMenuItem
-                            icon="lock"
-                            text="Change Password"
-                            onPress={() => { this.setState({ redirectToWeb: 'https://app.durhamgreentogo.com/account/change_password/' }) }}
-                        />
-                    </List>
-                    <SubscriptionBanner />
-                    <CommunityBoxes />
-                </Content>
-            )
         }
+        return (
+            <Content style={styles.container}>
+                <List>
+                    <ListMenuItem
+                        icon="person"
+                        text="View/Edit Name and Email"
+                        onPress={this.goToNameAndEmail}
+                    />
+                    <ListMenuItem
+                        icon="credit-card"
+                        text="Update payment method"
+                        onPress={() => { this.setState({ redirectToWeb: 'https://app.durhamgreentogo.com/account/change_payment_method/' }); }}
+                    />
+                    <ListMenuItem
+                        icon="inbox"
+                        text="View/Edit Subscriptions"
+                        onPress={() => { this.setState({ redirectToWeb: 'https://app.durhamgreentogo.com/subscriptions/' }); }}
+                    />
+                    <ListMenuItem
+                        icon="lock"
+                        text="Change Password"
+                        onPress={() => { this.setState({ redirectToWeb: 'https://app.durhamgreentogo.com/account/change_password/' }); }}
+                    />
+                </List>
+                <SubscriptionBanner />
+                <CommunityBoxes />
+            </Content>
+        );
     }
 }
 
