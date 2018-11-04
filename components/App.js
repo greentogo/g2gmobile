d5e0f5c26ce7fc114254ab4a51b0541c62f17918
Jonathan Canfield <jonathantcanfield@gmail.com>
2018-11-04 17:15:47 -0500

    lint App.js

--- components/App.js
+++ components/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Constants } from 'expo';
 import { observer, Provider } from 'mobx-react';
+import { createStackNavigator } from 'react-navigation';
 import styles from '../styles';
 import LoginScreen from './LoginScreen';
 import HomeScreen from './HomeScreen';
 import MapScreen from './MapScreen';
 import ScanQRCode from './ScanQRCode';
-import SubmissionScreen from "./SubmissionScreen";
-import ContainerSuccessScreen from "./ContainerSuccessScreen";
-import AccountScreen from "./AccountScreen";
-import SubscriptionScreen from "./SubscriptionScreen";
-import EditNameEmailScreen from "./EditNameEmailScreen";
+import SubmissionScreen from './SubmissionScreen';
+import ContainerSuccessScreen from './ContainerSuccessScreen';
+import AccountScreen from './AccountScreen';
+import SubscriptionScreen from './SubscriptionScreen';
+import EditNameEmailScreen from './EditNameEmailScreen';
 
-import { createStackNavigator } from 'react-navigation';
 
 const RootStack = createStackNavigator(
     {
@@ -23,7 +23,7 @@ const RootStack = createStackNavigator(
         containerSuccessScreen: ContainerSuccessScreen,
         account: AccountScreen,
         subscription: SubscriptionScreen,
-        editnameemail: EditNameEmailScreen
+        editnameemail: EditNameEmailScreen,
     },
     {
         initialRouteName: 'home',
@@ -38,30 +38,30 @@ const RootStack = createStackNavigator(
                 color: '#ffffff',
                 fontWeight: 'bold',
             },
-        }
-    }
+        },
+    },
 );
 
 @observer
 class App extends React.Component {
     componentDidMount() {
-        Expo.ScreenOrientation.allow(Expo.ScreenOrientation.Orientation.PORTRAIT_UP);
+        Expo.ScreenOrientation.allow(Expo.ScreenOrientation.Orientation.PORTRAIT_UP); // eslint-disable-line no-undef
     }
+
     render() {
-        const store = this.props.store;
+        const { store } = this.props;
         if (!store.authToken) {
             return <LoginScreen store={store} />;
-        } else {
-            return (
-                <Provider appStore={store}>
-                    <RootStack
-                        onNavigationStateChange={(prevState, newState) => {
-                            store.setCurrentRoute(newState)
-                        }}
-                    />
-                </Provider>
-            );
         }
+        return (
+            <Provider appStore={store}>
+                <RootStack
+                    onNavigationStateChange={(prevState, newState) => {
+                        store.setCurrentRoute(newState);
+                    }}
+                />
+            </Provider>
+        );
     }
 }
 
