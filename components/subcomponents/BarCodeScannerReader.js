575274eeec9e3098bc2775aa453102c506a814f9
Jonathan Canfield <jonathantcanfield@gmail.com>
2018-11-04 17:58:33 -0500

    lint BarCodeScannerReader, convert axios get to async await

--- components/subcomponents/BarCodeScannerReader.js
+++ components/subcomponents/BarCodeScannerReader.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { inject, observer } from "mobx-react";
+import { inject, observer } from 'mobx-react';
 import { Permissions, BarCodeScanner } from 'expo';
 import {
     Text,
-} from "native-base";
+} from 'native-base';
 import axios from '../../apiClient';
 
-@inject("appStore")
+@inject('appStore')
 @observer
 class BarCodeScannerReader extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             barCodeScanned: false,
             hasCameraPermission: false,
-        }
+        };
     }
 
     async componentDidMount() {
@@ -25,25 +25,29 @@ class BarCodeScannerReader extends React.Component {
 
     handleBarCodeRead = (data) => {
         if (!this.state.barCodeScanned) {
-            let authToken = this.props.appStore.authToken;
-            let url = JSON.stringify(data.data);
-            this.setState({ barCodeScanned: true }, () => {
-                let locationUrl = /(\/locations\/)([A-Z0-9]{6})/.exec(url);
+            const barcodeUrl = JSON.stringify(data.data);
+            this.setState({ barCodeScanned: true }, async () => {
+                const locationUrl = /(\/locations\/)([A-Z0-9]{6})/.exec(barcodeUrl);
                 if (locationUrl && locationUrl[1] && locationUrl[2]) {
-                    axios.get(locationUrl[1] + locationUrl[2], {
-                        headers: {
-                            'Authorization': `Token ${authToken}`
-                        }
-                    }).then((response) => {
+                    try {
+                        const url = `${locationUrl[1]}${locationUrl[2]}`;
+                        const config = {
+                            headers: {
+                                Authorization: `Token ${this.props.appStore.authToken}`,
+                            },
+                        };
+                        const response = await axios.get(url, config);
                         if (response.data && response.data.data && response.data.data.code) {
                             this.props.navigateNext(response.data.data);
                         } else {
                             this.setState({ barCodeScanned: false });
                         }
-                    }).catch((error) => {
-                        axios.post('/log/', { 'context': 'BarCodeScannerReader.js', 'error': error, 'message': error.message, 'stack': error.stack });
+                    } catch (error) {
+                        axios.post('/log/', {
+                            context: 'BarCodeScannerReader.js', error, message: error.message, stack: error.stack,
+                        });
                         this.setState({ barCodeScanned: false });
-                    })
+                    }
                 } else {
                     this.setState({ barCodeScanned: false });
                 }
@@ -61,9 +65,8 @@ class BarCodeScannerReader extends React.Component {
                     />
                 </View>
             );
-        } else {
-            return <Text> No camera view. Please give GreenToGo permission to access your camera so we can read QR codes! </Text>
         }
+        return <Text> No camera view. Please give GreenToGo permission to access your camera so we can read QR codes! </Text>;
     }
 }
 
