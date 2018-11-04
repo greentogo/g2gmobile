43319cf7bb46be2692c74a43f638b95487469f5a
Jonathan Canfield <jonathantcanfield@gmail.com>
2018-11-04 18:54:49 -0500

    lint ListMenuItem, convert to a pure function component

--- components/subcomponents/ListMenuItem.js
+++ components/subcomponents/ListMenuItem.js
@@ -9,36 +9,40 @@ import {
     ListItem,
     Text,
     Left,
-} from "native-base";
+} from 'native-base';
 import { MaterialIcons } from '@expo/vector-icons';
 import styles from '../../styles';
 
-class ListMenuItem extends React.Component {
-    render() {
-        const iconColor = this.props.color || styles.primaryColor;
-        const onPress = this.props.onPress || function () { };
-        const background = this.props.backgroundColor ?
-            {
-                borderRadius: 40,
-                width: 30,
-                height: 30,
-                backgroundColor: this.props.backgroundColor
-            } : null;
-        return (
-            <TouchableHighlight>
-                <ListItem style={{ flex: 1, height: 80, borderBottomWidth: StyleSheet.hairlineWidth, borderColor: styles.primaryColor, backgroundColor: styles.primaryCream }} icon onPress={onPress}>
-                    <Left>
-                        <View style={background}>
-                            <MaterialIcons style={{ color: iconColor }} size={30} name={this.props.icon} />
-                        </View>
-                    </Left>
-                    <Body style={{ borderBottomWidth: 0 }}>
-                        <Text>{this.props.text}</Text>
-                    </Body>
-                </ListItem>
-            </TouchableHighlight>
-        );
-    }
-}
+const ListMenuItem = (props) => {
+    const iconColor = props.color || styles.primaryColor;
+    const onPress = props.onPress || function () { };
+    const background = props.backgroundColor
+        ? {
+            borderRadius: 40,
+            width: 30,
+            height: 30,
+            backgroundColor: props.backgroundColor,
+        } : null;
+    return (
+        <TouchableHighlight>
+            <ListItem
+                style={{
+                    flex: 1, height: 80, borderBottomWidth: StyleSheet.hairlineWidth, borderColor: styles.primaryColor, backgroundColor: styles.primaryCream,
+                }}
+                icon
+                onPress={onPress}
+            >
+                <Left>
+                    <View style={background}>
+                        <MaterialIcons style={{ color: iconColor }} size={30} name={props.icon} />
+                    </View>
+                </Left>
+                <Body style={{ borderBottomWidth: 0 }}>
+                    <Text>{props.text}</Text>
+                </Body>
+            </ListItem>
+        </TouchableHighlight>
+    );
+};
 
 export default ListMenuItem;
