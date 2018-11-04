5cd3d46d47872da1536e27c5c97cbcac9960d081
Jonathan Canfield <jonathantcanfield@gmail.com>
2018-11-04 16:14:57 -0500

    lint subscriptionscreen and mark it as unused

--- components/SubscriptionScreen.js
+++ components/SubscriptionScreen.js
@@ -1,43 +1,52 @@
 import React from 'react';
 import {
     View,
-    Text
+    Text,
 } from 'react-native';
-import styles from "../styles";
+import styles from '../styles';
+
+// DISABLING ESLINT SINCE FILE IS UNUSED
+/* eslint-disable */
 
 class Subscription extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             subscriptions: [
                 { boxesAvailable: 2, totalBoxes: 4, expDate: '12/12/17' },
-                { boxesAvailable: 1, totalBoxes: 2, expDate: '2/5/19' }
+                { boxesAvailable: 1, totalBoxes: 2, expDate: '2/5/19' },
             ],
             availableBoxes: 0,
             totalBoxes: 0,
-            expDate: null
-        }
+            expDate: null,
+        };
     }
 
     componentDidMount() {
         this.setState({ availableBoxes: this.sumBoxes(this.state.subscriptions, 'boxesAvailable') });
-        this.setState({ totalBoxes: this.sumBoxes(this.state.subscriptions, 'totalBoxes') })
+        this.setState({ totalBoxes: this.sumBoxes(this.state.subscriptions, 'totalBoxes') });
     }
 
-    sumBoxes = (subscriptions, type) => {
-        return subscriptions.reduce((sum, subscription) => {
-            return sum + subscription[type];
-        }, 0);
-    }
+    sumBoxes = (subscriptions, type) => subscriptions.reduce((sum, subscription) => sum + subscription[type], 0)
 
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', flexDirection: 'row' }}>
-                <Text style={{ color: styles.primaryColor, fontWeight: 'bold', fontSize: 20 }}>You have {this.state.availableBoxes} of {this.state.totalBoxes} boxes available</Text>
+            <View style={{
+                flex: 1, alignItems: 'center', justifyContent: 'center', flexDirection: 'row',
+            }}
+            >
+                <Text style={{ color: styles.primaryColor, fontWeight: 'bold', fontSize: 20 }}>
+                    You have
+                    {this.state.availableBoxes}
+                    {' '}
+                    of
+                    {this.state.totalBoxes}
+                    {' '}
+                    boxes available
+                </Text>
             </View>
         );
     }
 }
 
 export default Subscription;
-
