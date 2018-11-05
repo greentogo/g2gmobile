ffdd4c3d524e2cd93fe4ca9b7798686957412919
Jonathan Canfield <jonathantcanfield@gmail.com>
2018-11-04 19:06:36 -0500

    lint G2GTitleImage and revert listmenuitem back to purecomponent

--- components/subcomponents/G2GTitleImage.js
+++ components/subcomponents/G2GTitleImage.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Image, View } from 'react-native';
+import styles from '../../styles';
 
+// PureComponents dont render as often as stateless functions, and are therefore faster
+/* eslint-disable react/prefer-stateless-function */
 class G2GTitleImage extends React.PureComponent {
     render() {
         return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <View style={styles.loadingContainer}>
                 <Image
                     source={require('../../assets/icons/g2g-white.png')}
                     style={{ height: 45, width: 144 }}
--- components/subcomponents/ListMenuItem.js
+++ components/subcomponents/ListMenuItem.js
@@ -13,36 +13,40 @@ import {
 import { MaterialIcons } from '@expo/vector-icons';
 import styles from '../../styles';
 
-const ListMenuItem = (props) => {
-    const iconColor = props.color || styles.primaryColor;
-    const onPress = props.onPress || function () { };
-    const background = props.backgroundColor
-        ? {
-            borderRadius: 40,
-            width: 30,
-            height: 30,
-            backgroundColor: props.backgroundColor,
-        } : null;
-    return (
-        <TouchableHighlight>
-            <ListItem
-                style={{
-                    flex: 1, height: 80, borderBottomWidth: StyleSheet.hairlineWidth, borderColor: styles.primaryColor, backgroundColor: styles.primaryCream,
-                }}
-                icon
-                onPress={onPress}
-            >
-                <Left>
-                    <View style={background}>
-                        <MaterialIcons style={{ color: iconColor }} size={30} name={props.icon} />
-                    </View>
-                </Left>
-                <Body style={{ borderBottomWidth: 0 }}>
-                    <Text>{props.text}</Text>
-                </Body>
-            </ListItem>
-        </TouchableHighlight>
-    );
-};
+// PureComponents dont render as often as stateless functions, and are therefore faster
+/* eslint-disable react/prefer-stateless-function */
+class ListMenuItem extends React.PureComponent {
+    render() {
+        const iconColor = this.props.color || styles.primaryColor;
+        const onPress = this.props.onPress || function () { };
+        const background = this.props.backgroundColor
+            ? {
+                borderRadius: 40,
+                width: 30,
+                height: 30,
+                backgroundColor: this.props.backgroundColor,
+            } : null;
+        return (
+            <TouchableHighlight>
+                <ListItem
+                    style={{
+                        flex: 1, height: 80, borderBottomWidth: StyleSheet.hairlineWidth, borderColor: styles.primaryColor, backgroundColor: styles.primaryCream,
+                    }}
+                    icon
+                    onPress={onPress}
+                >
+                    <Left>
+                        <View style={background}>
+                            <MaterialIcons style={{ color: iconColor }} size={30} name={this.props.icon} />
+                        </View>
+                    </Left>
+                    <Body style={{ borderBottomWidth: 0 }}>
+                        <Text>{this.props.text}</Text>
+                    </Body>
+                </ListItem>
+            </TouchableHighlight>
+        );
+    }
+}
 
 export default ListMenuItem;
