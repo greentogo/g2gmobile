0b7ae06ab3a3bd3d0724f7a66c721d000067a267
Jonathan Canfield <jonathantcanfield@gmail.com>
2018-11-04 20:32:00 -0500

    format apiClient

--- apiClient.js
+++ apiClient.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 const url = 'https://app.durhamgreentogo.com/api/v1'; // Production
 // const url = 'https://g2g.dreisbach.us/api/v1'; // Staging
-// const url = 'https://a4a168da.ngrok.io/api/v1'; // Testing
+// const url = 'https://4f5d7476.ngrok.io/api/v1'; // Testing
 
 const instance = axios.create({
-  baseURL: url,
-  timeout: 5000,
-  headers: {
-    Accept: 'application/json'
-  }
+    baseURL: url,
+    timeout: 5000,
+    headers: {
+        Accept: 'application/json',
+    },
 });
 
 instance.defaults.headers.post['Content-Type'] = 'application/json';
