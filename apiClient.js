80cf8a80c59a9231a84c76a3c97c6a58e04b22b2
Jonathan Canfield <jonathantcanfield@gmail.com>
2019-02-12 20:01:14 -0500

    Add android permissions and update api client

--- apiClient.js
+++ apiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const url = 'https://app.durhamgreentogo.com/api/v1'; // Production
 // const url = 'https://g2g.dreisbach.us/api/v1'; // Staging
-// const url = 'https://4f5d7476.ngrok.io/api/v1'; // Testing
+// const url = 'https://875fa750.ngrok.io/api/v1'; // Testing
 // TODO:
 // curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"https:..([^"]*).*/\1/p'
 // Then get it from process.env
