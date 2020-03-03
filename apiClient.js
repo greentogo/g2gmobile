23c473478428867c3c4089fd9427f343b5937dad
Rowe <gatlin.rowe@activision.com>
2020-03-03 18:41:32 -0500

    comment out dev code to allow debugging

--- apiClient.js
+++ apiClient.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
-import { NGROKHOST } from 'react-native-dotenv';
+// import { NGROKHOST } from 'react-native-dotenv';
 
 const prodHost = 'https://app.durhamgreentogo.com';
 // const url = 'https://g2g.dreisbach.us'; // Staging
 // const url = 'https://c1ad61be.ngrok.io'; // Testing
 
-const host = __DEV__ ? NGROKHOST || prodHost : prodHost;
+// const host = __DEV__ ? NGROKHOST || prodHost : prodHost;
+const host = prodHost;
 
 const instance = axios.create({
     baseURL: `${host}/api/v1`,
